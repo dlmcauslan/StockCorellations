f8100f4f4fadb1fbbd279aa86b512eea717a48d1
dlmcauslan <dlmcauslan@gmail.com>
2016-04-16 12:20:58 -0700

    Created Python file

---
+++ Stock_Corellations.py
@@ -0,0 +1,6 @@
+####                   Stock Corellations.py
+''' Data science project that downloads stock data from yahoo finance, and 
+calculates corellations over time.
+Created: 16/04/2016
+'''
+
